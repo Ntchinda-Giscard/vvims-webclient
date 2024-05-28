9e53a5b847e436b8058834e37c6a110176d1069a
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-05-28 18:18:48 +0100

    disable button

--- js/script.js
+++ js/script.js
@@ -4,15 +4,18 @@ document.getElementById('uploadForm').addEventListener('submit', function(event)
     const formData = new FormData(this);
     const endpoint = this.action; // Get the form action URL
 
-    // Show the loading spinner
+    // Show the loading spinner and disable the submit button
     document.getElementById('loading').style.display = 'block';
+    const submitButton = document.getElementById('submitButton');
+    submitButton.disabled = true;
 
     fetch(endpoint, {
         method: this.method,
         body: formData
     }).then(response => {
-        // Hide the loading spinner
+        // Hide the loading spinner and enable the submit button
         document.getElementById('loading').style.display = 'none';
+        submitButton.disabled = false;
 
         if (response.ok) {
             alert('Image uploaded successfully!');
@@ -20,8 +23,9 @@ document.getElementById('uploadForm').addEventListener('submit', function(event)
             alert('Failed to upload image.');
         }
     }).catch(error => {
-        // Hide the loading spinner
+        // Hide the loading spinner and enable the submit button
         document.getElementById('loading').style.display = 'none';
+        submitButton.disabled = false;
 
         console.error('Error:', error);
         alert('An error occurred.');
